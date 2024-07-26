093b3e211c809e10f2a13b423707ecbc403f4334
selvabharathi M <bselva609@gmail.com>
2024-07-26 11:46:23 +0530

    ubdated

--- secondMax.java
+++ secondMax.java
@@ -2,16 +2,14 @@
 public class secondMax {
     public static void main(String[] args) {
         int arr[] = { 1, 5, 3, 1, 3 };
-        int max = arr[0];
+        int max = Integer.MIN_VALUE;
+         int max2 =Integer.MIN_VALUE;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] >max) {
                 max = arr[i];
             }
-        }
-        int max2 =arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max2 && arr[i] != max) {
-                max2= arr[i];
+            else if(arr[i] > max2 && arr[i] != max){
+                 max2= arr[i];
             }
         }
         System.out.println(max);
